073a22740690ac080cab6047781ca0f311c11784
harshG775 <hgaur491@gmail.com>
2026-04-13 17:29:58 +0530

    feat: implement theme context and themed components for light/dark mode support

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,28 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { ThemeModeProvider, useThemeModeContext } from "@/src/components/contexts/theme-mode-provider";
+import { ThemeProvider } from "@/src/components/contexts/theme-provider";
 
 export default function RootLayout() {
-    return <Stack />;
+    return (
+        <SafeAreaProvider>
+            <ThemeModeProvider>
+                <ThemeProvider>
+                    {/* <Stack>
+                        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                    </Stack> */}
+                    <Stack />
+                    <StatusBar_ />
+                </ThemeProvider>
+            </ThemeModeProvider>
+        </SafeAreaProvider>
+    );
+}
+function StatusBar_() {
+    const { mode } = useThemeModeContext();
+
+    return <StatusBar style={mode === "dark" ? "light" : "dark"} />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
-
+import { useThemeModeContext } from "../components/contexts/theme-mode-provider";
+import { ThemedText } from "../components/core/themed-text";
+import { ThemedView } from "../components/core/themed-view";
+import Button from "../components/ui/button";
 export default function Index() {
+    const { mode, setMode } = useThemeModeContext();
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Edit app/index.tsx to edit this screen.</Text>
-        </View>
+        <ThemedView style={{ flex: 1, paddingHorizontal: 8, paddingVertical: 12 }}>
+            <Button
+                onPress={() => {
+                    setMode(mode === "light" ? "dark" : "light");
+                }}
+                title={mode === "light" ? "Dark" : "Light"}
+                icon={mode === "light" ? "dark-mode" : "light-mode"}
+                style={{
+                    justifyContent: "flex-start",
+                }}
+            />
+
+            <ThemedText style={{ marginTop: 40 }}>Current mode: {mode}</ThemedText>
+        </ThemedView>
     );
 }
